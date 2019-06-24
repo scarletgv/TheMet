0363a9a0e3cdad5a51d89ab810aca407bced5b29
Stormers16 <33431165+Stormers16@users.noreply.github.com>
2019-06-23 22:25:24 -0300

    Nao consigo consultar cm country="Brasil"

---
+++ consultas/objetos_brasileiros.sql
@@ -0,0 +1,10 @@
+SELECT 
+    *
+FROM
+    country_object
+        NATURAL JOIN
+    country
+where countryid =35 OR countryid = 100 
+
+#Por algum motivo, nao consigo consultar com country="Brasil"
+
--- consultas/tipo_de_objeto_mais_comum_de_origem_brasileira.sql
+++ consultas/tipo_de_objeto_mais_comum_de_origem_brasileira.sql
@@ -1,15 +1,14 @@
 SELECT 
-    *,
     object.objectname AS 'Tipo de Objeto',
     COUNT(0) AS `Número de objetos desse tipo`
 FROM
     object
         NATURAL JOIN
-    (SELECT 
-        objectid
-    FROM
-        country_object
-    NATURAL JOIN country
-    where country.country="Brazil") as objetos_brasileiros
+    (SELECT *
+	FROM
+		country_object
+			NATURAL JOIN
+		country
+	where countryid =35 OR countryid = 100 ) as objetos_brasileiros
 GROUP BY object.objectname
 ORDER BY COUNT(0) DESC
