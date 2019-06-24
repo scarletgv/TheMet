2fb4de460fbf671c0fd8d127f6a803071871f00c
Stormers16 <33431165+Stormers16@users.noreply.github.com>
2019-06-24 11:37:46 -0300

    Brazil

--- consultas/consultas.sql
+++
@@ -1,2 +0,0 @@
-SELECT * from artist
-group by name;
--- consultas/objetos_brasileiros.sql
+++ consultas/objetos_brasileiros.sql
@@ -5,6 +5,5 @@ FROM
         NATURAL JOIN
     country
 where countryid =35 OR countryid = 100 
-
-#Por algum motivo, nao consigo consultar com country="Brasil"
+#Por algum motivo, nao consigo consultar com country="Brazil"
 
