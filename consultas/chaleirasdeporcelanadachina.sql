7137d5cf4f2b1b3b60ee68feb44ace4c20e83e60
Stormers16 <33431165+Stormers16@users.noreply.github.com>
2019-06-25 22:07:24 -0300

    limpeza

--- consultas/chaleirasdeporcelanadachina.sql
+++ consultas/chaleirasdeporcelanadachina.sql
@@ -1,4 +1,14 @@
-SELECT object.OBJECTNAME, object.medium, primaryimage
-	FROM 
-		object NATURAL JOIN image_object NATURAL JOIN country_object NATURAL JOIN country
-WHERE countryid = 8 AND object.medium = 'Porcelain' AND OBJECTNAME = 'Teapot'
+SELECT 
+    object.OBJECTNAME, object.medium, primaryimage
+FROM
+    object
+        NATURAL JOIN
+    image_object
+        NATURAL JOIN
+    country_object
+        NATURAL JOIN
+    country
+WHERE
+    countryid = 8
+        AND object.medium = 'Porcelain'
+        AND OBJECTNAME = 'Teapot'
--- consultas/vangogh.sql
+++ consultas/vangogh.sql
@@ -1,4 +1,12 @@
-SELECT object.OBJECTNAME, image_object.PRIMARYIMAGE 
-	FROM 
-		object NATURAL JOIN image_object NATURAL JOIN artist_object NATURAL JOIN artist 
-	WHERE artist.ARTISTID = 14677
+SELECT 
+    object.OBJECTNAME, image_object.PRIMARYIMAGE
+FROM
+    object
+        NATURAL JOIN
+    image_object
+        NATURAL JOIN
+    artist_object
+        NATURAL JOIN
+    artist
+WHERE
+    artist.ARTISTID = 14677
