57fe915e229131bb1708bb0769f8e01d2b424e70
scarletgv <33155764+scarletgv@users.noreply.github.com>
2019-06-26 17:29:11 -0300

    Delete doacoes-por-ano-museu.sql

--- consultas/consultas-relatorio/doacoes-por-ano-museu.sql
+++
@@ -1,6 +0,0 @@
-SELECT donor_object.YEAR AS Ano, COUNT(donor_object.OBJECTID) AS Doações
-	FROM object NATURAL JOIN donor_object
-		WHERE 
-			donor_object.YEAR > 1871 AND donor_object.YEAR < 2018
-GROUP BY donor_object.YEAR
-ORDER BY donor_object.YEAR
